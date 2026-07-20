ec292deb63a71ed9848892d897230cb08b19bb59
Barnz <hi@barnz.dev>
2026-07-20 18:23:52 +0100

    set up devenv

---
+++ devenv.nix
@@ -0,0 +1,22 @@
+{pkgs, lib, ...}:
+let
+  pyproject = builtins.fromTOML (builtins.readFile ./pyproject.toml);
+  requirements = pkgs.writeText "requirements.txt" (
+    lib.concatStringsSep "\n" pyproject.project.dependencies + "\n"
+  );
+in {
+  packages = [
+    pkgs.pyright
+    pkgs.python313Packages.debugpy
+  ];
+
+  languages.python = {
+    enable = true;
+    package = pkgs.python313;
+
+    venv = {
+      enable = true;
+      requirements = requirements;
+    };
+  };
+}
